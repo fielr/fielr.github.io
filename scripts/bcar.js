24fc4d9695412af06ba9bea9fc59e2a95696100c
fielr <61761822+fielr@users.noreply.github.com>
2024-01-07 11:02:23 +0900

    Add files via upload

--- scripts/bcar.js
+++ scripts/bcar.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name BCAR
+// @namespace https://www.bondageprojects.com/
+// @version 0.7.5
+// @description BCAR Bondacge Club Auto React
+// @author DrBranestawm
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at document-end
+// @grant none
+// ==/UserScript==
+
 const BCAR_Version = '0.7.5';
 const BCAR_Version_FIX = '';
 
