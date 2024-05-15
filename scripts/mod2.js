95759afe039a0ee622501320e3f6959f178bc1c0
fielr <61761822+fielr@users.noreply.github.com>
2024-05-15 17:07:27 +0900

    Update mod2.js

--- scripts/mod2.js
+++ scripts/mod2.js
@@ -16,7 +16,7 @@
     var bcModSdk = function () {
         "use strict"; const o = "1.2.0"; function e(o) { alert("Mod ERROR:\n" + o); const e = new Error(o); throw console.error(e), e } const t = new TextEncoder; function n(o) { return !!o && "object" == typeof o && !Array.isArray(o) } function r(o) { const e = new Set; return o.filter((o => !e.has(o) && e.add(o))) } const i = new Map, a = new Set; function c(o) { a.has(o) || (a.add(o), console.warn(o)) } function s(o) { const e = [], t = new Map, n = new Set; for (const r of f.values()) { const i = r.patching.get(o.name); if (i) { e.push(...i.hooks); for (const [e, a] of i.patches.entries()) t.has(e) && t.get(e) !== a && c(`ModSDK: Mod '${r.name}' is patching function ${o.name} with same pattern that is already applied by different mod, but with different pattern:\nPattern:\n${e}\nPatch1:\n${t.get(e) || ""}\nPatch2:\n${a}`), t.set(e, a), n.add(r.name) } } e.sort(((o, e) => e.priority - o.priority)); const r = function (o, e) { if (0 === e.size) return o; let t = o.toString().replaceAll("\r\n", "\n"); for (const [n, r] of e.entries()) t.includes(n) || c(`ModSDK: Patching ${o.name}: Patch ${n} not applied`), t = t.replaceAll(n, r); return (0, eval)(`(${t})`) }(o.original, t); let i = function (e) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookChainExit) || void 0 === i ? void 0 : i.call(t, o.name, n), c = r.apply(this, e); return null == a || a(), c }; for (let t = e.length - 1; t >= 0; t--) { const n = e[t], r = i; i = function (e) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookEnter) || void 0 === i ? void 0 : i.call(t, o.name, n.mod), c = n.hook.apply(this, [e, o => { if (1 !== arguments.length || !Array.isArray(e)) throw new Error(`Mod ${n.mod} failed to call next hook: Expected args to be array, got ${typeof o}`); return r.call(this, o) }]); return null == a || a(), c } } return { hooks: e, patches: t, patchesSources: n, enter: i, final: r } } function l(o, e = !1) { let r = i.get(o); if (r) e && (r.precomputed = s(r)); else { let e = window; const a = o.split("."); for (let t = 0; t < a.length - 1; t++)if (e = e[a[t]], !n(e)) throw new Error(`ModSDK: Function ${o} to be patched not found; ${a.slice(0, t + 1).join(".")} is not object`); const c = e[a[a.length - 1]]; if ("function" != typeof c) throw new Error(`ModSDK: Function ${o} to be patched not found`); const l = function (o) { let e = -1; for (const n of t.encode(o)) { let o = 255 & (e ^ n); for (let e = 0; e < 8; e++)o = 1 & o ? -306674912 ^ o >>> 1 : o >>> 1; e = e >>> 8 ^ o } return ((-1 ^ e) >>> 0).toString(16).padStart(8, "0").toUpperCase() }(c.toString().replaceAll("\r\n", "\n")), d = { name: o, original: c, originalHash: l }; r = Object.assign(Object.assign({}, d), { precomputed: s(d), router: () => { }, context: e, contextProperty: a[a.length - 1] }), r.router = function (o) { return function (...e) { return o.precomputed.enter.apply(this, [e]) } }(r), i.set(o, r), e[r.contextProperty] = r.router } return r } function d() { for (const o of i.values()) o.precomputed = s(o) } function p() { const o = new Map; for (const [e, t] of i) o.set(e, { name: e, original: t.original, originalHash: t.originalHash, sdkEntrypoint: t.router, currentEntrypoint: t.context[t.contextProperty], hookedByMods: r(t.precomputed.hooks.map((o => o.mod))), patchedByMods: Array.from(t.precomputed.patchesSources) }); return o } const f = new Map; function u(o) { f.get(o.name) !== o && e(`Failed to unload mod '${o.name}': Not registered`), f.delete(o.name), o.loaded = !1, d() } function g(o, t) { o && "object" == typeof o || e("Failed to register mod: Expected info object, got " + typeof o), "string" == typeof o.name && o.name || e("Failed to register mod: Expected name to be non-empty string, got " + typeof o.name); let r = `'${o.name}'`; "string" == typeof o.fullName && o.fullName || e(`Failed to register mod ${r}: Expected fullName to be non-empty string, got ${typeof o.fullName}`), r = `'${o.fullName} (${o.name})'`, "string" != typeof o.version && e(`Failed to register mod ${r}: Expected version to be string, got ${typeof o.version}`), o.repository || (o.repository = void 0), void 0 !== o.repository && "string" != typeof o.repository && e(`Failed to register mod ${r}: Expected repository to be undefined or string, got ${typeof o.version}`), null == t && (t = {}), t && "object" == typeof t || e(`Failed to register mod ${r}: Expected options to be undefined or object, got ${typeof t}`); const i = !0 === t.allowReplace, a = f.get(o.name); a && (a.allowReplace && i || e(`Refusing to load mod ${r}: it is already loaded and doesn't allow being replaced.\nWas the mod loaded multiple times?`), u(a)); const c = o => { let e = g.patching.get(o.name); return e || (e = { hooks: [], patches: new Map }, g.patching.set(o.name, e)), e }, s = (o, t) => (...n) => { var i, a; const c = null === (a = (i = m.errorReporterHooks).apiEndpointEnter) || void 0 === a ? void 0 : a.call(i, o, g.name); g.loaded || e(`Mod ${r} attempted to call SDK function after being unloaded`); const s = t(...n); return null == c || c(), s }, p = { unload: s("unload", (() => u(g))), hookFunction: s("hookFunction", ((o, t, n) => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const i = l(o), a = c(i); "number" != typeof t && e(`Mod ${r} failed to hook function '${o}': Expected priority number, got ${typeof t}`), "function" != typeof n && e(`Mod ${r} failed to hook function '${o}': Expected hook function, got ${typeof n}`); const s = { mod: g.name, priority: t, hook: n }; return a.hooks.push(s), d(), () => { const o = a.hooks.indexOf(s); o >= 0 && (a.hooks.splice(o, 1), d()) } })), patchFunction: s("patchFunction", ((o, t) => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const i = l(o), a = c(i); n(t) || e(`Mod ${r} failed to patch function '${o}': Expected patches object, got ${typeof t}`); for (const [n, i] of Object.entries(t)) "string" == typeof i ? a.patches.set(n, i) : null === i ? a.patches.delete(n) : e(`Mod ${r} failed to patch function '${o}': Invalid format of patch '${n}'`); d() })), removePatches: s("removePatches", (o => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const t = l(o); c(t).patches.clear(), d() })), callOriginal: s("callOriginal", ((o, t, n) => { "string" == typeof o && o || e(`Mod ${r} failed to call a function: Expected function name string, got ${typeof o}`); const i = l(o); return Array.isArray(t) || e(`Mod ${r} failed to call a function: Expected args array, got ${typeof t}`), i.original.apply(null != n ? n : globalThis, t) })), getOriginalHash: s("getOriginalHash", (o => { "string" == typeof o && o || e(`Mod ${r} failed to get hash: Expected function name string, got ${typeof o}`); return l(o).originalHash })) }, g = { name: o.name, fullName: o.fullName, version: o.version, repository: o.repository, allowReplace: i, api: p, loaded: !0, patching: new Map }; return f.set(o.name, g), Object.freeze(p) } function h() { const o = []; for (const e of f.values()) o.push({ name: e.name, fullName: e.fullName, version: e.version, repository: e.repository }); return o } let m; const y = void 0 === window.bcModSdk ? window.bcModSdk = function () { const e = { version: o, apiVersion: 1, registerMod: g, getModsInfo: h, getPatchingInfo: p, errorReporterHooks: Object.seal({ apiEndpointEnter: null, hookEnter: null, hookChainExit: null }) }; return m = e, Object.freeze(e) }() : (n(window.bcModSdk) || e("Failed to init Mod SDK: Name already in use"), 1 !== window.bcModSdk.apiVersion && e(`Failed to init Mod SDK: Different version already loaded ('1.2.0' vs '${window.bcModSdk.version}')`), window.bcModSdk.version !== o && alert(`Mod SDK warning: Loading different but compatible versions ('1.2.0' vs '${window.bcModSdk.version}')\nOne of mods you are using is using an old version of SDK. It will work for now but please inform author to update`), window.bcModSdk); return "undefined" != typeof exports && (Object.defineProperty(exports, "__esModule", { value: !0 }), exports.default = y), y
     }();
-    
+
     const MOD_NAME = "动作拓展";
     const MOD_FULL_NAME = "动作拓展";
     const MOD_VERSION = "0.3.2";
@@ -458,14 +458,6 @@
             name: "脚托起下巴", prerequisite: ["UseFeet"],
             targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
             target: "ItemMouth", targettext: "SourceCharacter用脚托起TargetCharacter的下巴.", maxProgress: 50,
-            activityExpression: [],
-            assetgroup: "", imageName: "Wiggle",
-            modPosture: false, modifyOwnPosture: false, postureName: ""
-        },
-        {
-            name: "踩住头", prerequisite: ["canStepOnHead"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
-            target: "ItemHead", targettext: "SourceCharacter踩住TargetCharacter的头.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "Step",
             modPosture: false, modifyOwnPosture: false, postureName: ""
@@ -1143,6 +1135,14 @@
             modPosture: false, modifyOwnPosture: false, postureName: ""
         },
         {
+            name: "触手掀裙子", prerequisite: ["HasTentacles2"],
+            targetSelf: "ItemButt", targetSelftext: "SourceCharacter用触手掀开PronounPossessive的裙子.", maxProgressSelf: 50,
+            target: "ItemButt", targettext: "SourceCharacter用触手掀开TargetCharacter的裙子.", maxProgress: 50,
+            activityExpression: [],
+            assetgroup: "TailStraps", imageName: "Tentacles",
+            modPosture: false, modifyOwnPosture: false, postureName: ""
+        },
+        {
             name: "触手揉脸", prerequisite: ["HasTentacles2"],
             targetSelf: "ItemMouth", targetSelftext: "SourceCharacter用触手揉了揉自己的脸.", maxProgressSelf: 50,
             target: "ItemMouth", targettext: "SourceCharacter用触手揉了揉TargetCharacter的脸.", maxProgress: 50,
@@ -1240,7 +1240,7 @@
         },
         {
             name: "射击乳房", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemBreast", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的乳房.", maxProgressSelf: 50,
             target: "ItemBreast", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的乳房.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1248,7 +1248,7 @@
         },
         {
             name: "射击屁股", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemButt", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的屁股.", maxProgressSelf: 50,
             target: "ItemButt", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的屁股.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1256,7 +1256,7 @@
         },
         {
             name: "射击脸", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemMouth", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的脸.", maxProgressSelf: 50,
             target: "ItemMouth", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的脸.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1264,7 +1264,7 @@
         },
         {
             name: "射击腰", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemTorso", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的腰.", maxProgressSelf: 50,
             target: "ItemTorso", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的腰.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1272,7 +1272,7 @@
         },
         {
             name: "射击耳朵", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemEars", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的耳朵.", maxProgressSelf: 50,
             target: "ItemEars", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的耳朵.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1280,7 +1280,7 @@
         },
         {
             name: "射击手臂", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemArms", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的手臂.", maxProgressSelf: 50,
             target: "ItemArms", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的手臂.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1288,7 +1288,7 @@
         },
         {
             name: "射击脖子", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemNeck", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的脖子.", maxProgressSelf: 50,
             target: "ItemNeck", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的脖子.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1296,7 +1296,7 @@
         },
         {
             name: "射击头", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemHead", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的头.", maxProgressSelf: 50,
             target: "ItemHead", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的头.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1304,7 +1304,7 @@
         },
         {
             name: "射击眉心", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemHead", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的眉心.", maxProgressSelf: 50,
             target: "ItemHead", targettext: "SourceCharacter举起枪瞄准, 水弹正中TargetCharacter的眉心!", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1312,7 +1312,7 @@
         },
         {
             name: "射击鼻子", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemNose", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的鼻子.", maxProgressSelf: 50,
             target: "ItemNose", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的鼻子.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1320,7 +1320,7 @@
         },
         {
             name: "射击肚子", prerequisite: ["阿巴阿巴"],
-            targetSelf: "", targetSelftext: "", maxProgressSelf: 50,
+            targetSelf: "ItemPelvis", targetSelftext: "SourceCharacter举起枪瞄准PronounPossessive自己的肚子.", maxProgressSelf: 50,
             target: "ItemPelvis", targettext: "SourceCharacter举起枪瞄准, 水弹直直击中了TargetCharacter的肚子.", maxProgress: 50,
             activityExpression: [],
             assetgroup: "", imageName: "阿巴阿巴",
@@ -1370,7 +1370,6 @@
         "HasTentacles2": (acting, acted, group) => !!InventoryIsItemInList(acting, "TailStraps", "Tentacles"), // 触手
         "SuitLower鱼鱼尾_Luzi": (acting, acted, group) => !!InventoryIsItemInList(acting, "SuitLower", "鱼鱼尾_Luzi"),
         "阿巴阿巴": (acting, acted, group) => !!InventoryIsItemInList(acting, "ItemHandheld", "阿巴阿巴_Luzi"),
-        "canStepOnHead": (acting, acted, group) => acting.CanWalk() && acting.IsStanding() && (acted.IsKneeling() || ["Hogtied", "AllFours"].includes(acted.PoseMapping.BodyLower)),
     }));
 
     笨蛋Luzi.hookFunction("ActivityCheckPrerequisite", 6, (args, next) => {
@@ -1462,7 +1461,7 @@
         ["SourceCharacter wraps PronounPossessive arms around TargetCharacter in a big warm hug.", "SourceCharacter用温暖的拥抱将PronounPossessive的手臂紧紧地环绕在TargetCharacter身上."],
         ["SourceCharacter wraps TargetCharacter in a therapeutic selfhug.", "SourceCharacter给TargetCharacter一个治疗性的自我拥抱."],
         ["Tackle", "扑倒"],
-        ["SourceCharacter full body tackles TargetCharacter!", "SourceCharacter用全身扑在TargetCharacter身上!"],
+        ["SourceCharacter full body tackles TargetCharacter!", "SourceCharacter全身扑到TargetCharacter身上."],
         ["Flop", "瘫倒"],
         ["SourceCharacter flops on top of TargetCharacter.", "SourceCharacter瘫倒在TargetCharacter的身上."],
         ["Kiss Eyes", "亲吻眼睛"],
