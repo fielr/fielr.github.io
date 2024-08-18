70bd20d8882ae19054617914d38fced070899055
fielr <61761822+fielr@users.noreply.github.com>
2024-08-18 21:29:48 +0900

    update wce

--- scripts/wce/wce-fusam-loader.user.js
+++ scripts/wce/wce-fusam-loader.user.js
@@ -1,34 +1,32 @@
 // ==UserScript==
-// @name WCE loader local with FUSAM
+// @name WCE local loader with FUSAM
 // @namespace https://www.bondageprojects.com/
-// @version 1.3
+// @version 1.4
 // @description Wholesome Club Extensions (WCE) - enhancements for the bondage club - fork of FBC 5.8
 // @author Sidious, Stella
 // @supportURL https://github.com/KittenApps/WCE
-// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
 // @match http://localhost:*/*
 // @icon https://wce-docs.vercel.app/img/logo.png
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
-import(`https://sidiousious.gitlab.io/bc-addon-loader/fusam.js?v=${(Date.now()/10000).toFixed(0)}`);
+var URL = 'http://localhost:4000';
+
+import(`https://sidiousious.gitlab.io/bc-addon-loader/fusam.js?v=${(Date.now()/10000).toFixed(0)}`).then(() => import(`${URL}/wce.js`));
 
 var preloadLink = document.createElement("link");
-preloadLink.href = 'https://fielr.github.io/scripts/wce/wce.js';
+preloadLink.href = `${URL}/wce.js`;
 preloadLink.rel = "modulepreload";
 document.head.appendChild(preloadLink);
 
 var dexiePreloadLink = document.createElement("link");
-dexiePreloadLink.href = 'https://fielr.github.io/scripts/wce/dexie.js';
+dexiePreloadLink.href = `${URL}/dexie.js`;
 dexiePreloadLink.rel = "modulepreload";
 document.head.appendChild(dexiePreloadLink);
 
 var fusam = JSON.parse(localStorage.getItem("fusam.settings") || "{}");
 fusam.enabledDistributions ??= {};
-fusam.enabledDistributions.WCE ??= "stable";
+delete fusam.enabledDistributions.WCE;
 delete fusam.enabledDistributions.FBC;
 localStorage.setItem("fusam.settings", JSON.stringify(fusam));
--- scripts/wce/wce-loader.user.js
+++ scripts/wce/wce-loader.user.js
@@ -1,35 +1,42 @@
 // ==UserScript==
-// @name WCE loader local
+// @name WCE local loader
 // @namespace https://www.bondageprojects.com/
-// @version 1.0
+// @version 1.1
 // @description Wholesome Club Extensions (WCE) - enhancements for the bondage club - fork of FBC 5.8
 // @author Sidious, Stella
 // @supportURL https://github.com/KittenApps/WCE
-// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
 // @match http://localhost:*/*
 // @icon https://wce-docs.vercel.app/img/logo.png
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
-var SCRIPT_URL = 'http://localhost:4000/wce.js';
+var URL = 'http://localhost:4000';
 
 var preloadLink = document.createElement("link");
-preloadLink.href = SCRIPT_URL;
+preloadLink.href = `${URL}/wce.js`;
 preloadLink.rel = "modulepreload";
 document.head.appendChild(preloadLink);
 
+var dexiePreloadLink = document.createElement("link");
+dexiePreloadLink.href = `${URL}/dexie.js`;
+dexiePreloadLink.rel = "modulepreload";
+document.head.appendChild(dexiePreloadLink);
+
+var fusam = JSON.parse(localStorage.getItem("fusam.settings") || "{}");
+fusam.enabledDistributions ??= {};
+delete fusam.enabledDistributions.WCE;
+delete fusam.enabledDistributions.FBC;
+localStorage.setItem("fusam.settings", JSON.stringify(fusam));
+
 if (typeof FUSAM === "object" && FUSAM?.present) {
-  import(SCRIPT_URL);
+  import(`${URL}/wce.js`);
 } else {
   let storeFUSAM;
   Object.defineProperty(window, "FUSAM", {
     set(n) {
       storeFUSAM = n;
-      import(SCRIPT_URL);
+      import(`${URL}/wce.js`);
     },
     get() {
       return storeFUSAM;
